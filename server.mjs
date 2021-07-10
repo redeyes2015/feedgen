4bab5e14c20c3a25ecdbcbd9df3ea98e5461f384
redeyes2015 <redeyes0215@gmail.com>
2021-07-10 12:10:02 +0800

    Specify port

--- server.mjs
+++ server.mjs
@@ -13,14 +13,16 @@ fastify.get('/bookwalker_atom.xml', () => {
   return genBookWalkerFeed('https://www.bookwalker.com.tw/category/3/28?order=sell_desc')
 });
 
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 80;
+console.log({ port });
+
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(2015)
+    await fastify.listen(port)
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
   }
 }
 start()
-
