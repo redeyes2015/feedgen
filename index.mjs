647746b37c0de2e4e9ae7ef0409922236d1a4b27
redeyes2015 <redeyes0215@gmail.com>
2022-02-08 17:54:10 +0800

    🩹 Update `index.mjs`

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
-import { genBookWalkerFeed } from './genBookWalkerFeed.mjs';
+import genBookWalkerFeed from './genBookWalkerFeed.mjs';
 
 // const url = 'https://www.bookwalker.com.tw/more/fiction/1/3',
-const url = 'https://www.bookwalker.com.tw/category/3/28?order=sell_desc';
+// const url = 'https://www.bookwalker.com.tw/category/3/28?order=sell_desc';
+const url = 'https://www.bookwalker.com.tw/search?order=sell_desc&m=3&s=28';
 
-console.log(genBookWalkerFeed(url));
+console.log(await genBookWalkerFeed(url));
