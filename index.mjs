a712a029d1ffcc0fd1806df0961f6081fddacce0
redeyes2015 <redeyes0215@gmail.com>
2023-06-21 14:32:12 +0800

    Use `node:fs.writeFile` instead of shell redirect

--- index.mjs
+++ index.mjs
@@ -1,7 +1,15 @@
+import { writeFile } from 'node:fs/promises';
 import genBookWalkerFeed from './genBookWalkerFeed.mjs';
 
-// const url = 'https://www.bookwalker.com.tw/more/fiction/1/3',
-// const url = 'https://www.bookwalker.com.tw/category/3/28?order=sell_desc';
-const url = 'https://www.bookwalker.com.tw/search?order=sell_desc&m=3&s=28';
+const bookWalkerJob = async () => {
+    // const url = 'https://www.bookwalker.com.tw/more/fiction/1/3',
+    // const url = 'https://www.bookwalker.com.tw/category/3/28?order=sell_desc';
+    const url = 'https://www.bookwalker.com.tw/search?order=sell_desc&m=3&s=28';
+    const feedData = await genBookWalkerFeed(url);
+    return writeFile('public/bookwalker_atom.xml', feedData);
+};
 
-console.log(await genBookWalkerFeed(url));
+await bookWalkerJob();
+
+
+
